fba6a71bebfafbad1f36d76c27fd0fc129d3faa8
Tim <tim@example.com>
2025-11-07 23:46:50 -0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+    open: true,
+  },
+});
