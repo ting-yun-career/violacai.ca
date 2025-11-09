fd6855295928dedb51cface74610f89fd89ff433
Tim <tim@example.com>
2025-11-08 16:47:16 -0800

    update

--- js/main.js
+++ js/main.js
@@ -40,26 +40,6 @@ import { SplitText } from "gsap/SplitText";
       ease: "elastic.out(1, 0.5)",
     });
 
-    $(".sidebar .list-menu")
-      .clone()
-      .children()
-      .appendTo(".mobile-navbar .navbar-nav")
-      .find(".nav-link")
-      .removeClass("active");
-
-    $(document).on("mouseup", function (event) {
-      if ($(".mobile-navbar #mobileNavbarSupportedContent").hasClass("show")) {
-        // The mobile Bootstrap navbar dropdown
-        var navbarToggler = $(".mobile-navbar .navbar-toggler");
-        if (
-          !navbarToggler.is(event.target) &&
-          navbarToggler.has(event.target).length === 0
-        ) {
-          navbarToggler.trigger("click");
-        }
-      }
-    });
-
     (function () {
       var animationDelay = 2500,
         revealDuration = 660,
