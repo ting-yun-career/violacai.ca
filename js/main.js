62eb69d4f5128f7c4b4747f5a6f70ed547e2c515
Tim <tim@example.com>
2025-11-08 16:49:28 -0800

    update

--- js/main.js
+++ js/main.js
@@ -118,232 +118,116 @@ import { SplitText } from "gsap/SplitText";
       }
     })();
 
-    if ($(".home-area").hasClass("snow-variant")) {
-      particlesJS("particles-js", {
-        particles: {
-          number: {
-            value: 50,
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-          },
-          color: {
-            value: "#fff",
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#000000",
-            },
-            polygon: {
-              nb_sides: 5,
-            },
-            image: {
-              src: "img/github.svg",
-              width: 100,
-              height: 100,
-            },
-          },
-          opacity: {
-            value: 0.5,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false,
-            },
-          },
-          size: {
-            value: 8,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 40,
-              size_min: 0.1,
-              sync: false,
-            },
-          },
-          line_linked: {
-            enable: false,
-            distance: 500,
-            color: "#ffffff",
-            opacity: 0.4,
-            width: 2,
-          },
-          move: {
+    particlesJS("particles-js", {
+      particles: {
+        number: {
+          value: 4,
+          density: {
             enable: true,
-            speed: 3,
-            direction: "bottom",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 1200,
-            },
+            value_area: 800,
           },
         },
-        interactivity: {
-          detect_on: "canvas",
-          events: {
-            onhover: {
-              enable: false,
-              mode: "bubble",
-            },
-            onclick: {
-              enable: false,
-              mode: "repulse",
-            },
-            resize: true,
+        color: {
+          value: "#ffffff",
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000",
           },
-          modes: {
-            grab: {
-              distance: 400,
-              line_linked: {
-                opacity: 0.5,
-              },
-            },
-            bubble: {
-              distance: 400,
-              size: 4,
-              duration: 0.3,
+          polygon: {
+            nb_sides: 6,
+          },
+          image: {
+            src: "img/github.svg",
+            width: 100,
+            height: 100,
+          },
+        },
+        opacity: {
+          value: 0.2,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false,
+          },
+        },
+        size: {
+          value: 90,
+          random: false,
+          anim: {
+            enable: true,
+            speed: 10,
+            size_min: 40,
+            sync: false,
+          },
+        },
+        line_linked: {
+          enable: false,
+          distance: 200,
+          color: "#ffffff",
+          opacity: 1,
+          width: 2,
+        },
+        move: {
+          enable: true,
+          speed: 8,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: false,
+            mode: "grab",
+          },
+          onclick: {
+            enable: false,
+            mode: "push",
+          },
+          resize: true,
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
               opacity: 1,
-              speed: 3,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-            push: {
-              particles_nb: 4,
-            },
-            remove: {
-              particles_nb: 2,
             },
           },
-        },
-        retina_detect: true,
-      });
-    }
-
-    // If bubble variant
-    else if ($(".home-area").hasClass("bubble-variant")) {
-      particlesJS("particles-js", {
-        particles: {
-          number: {
-            value: 4,
-            density: {
-              enable: true,
-              value_area: 800,
-            },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3,
           },
-          color: {
-            value: "#ffffff",
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 0,
-              color: "#000",
-            },
-            polygon: {
-              nb_sides: 6,
-            },
-            image: {
-              src: "img/github.svg",
-              width: 100,
-              height: 100,
-            },
-          },
-          opacity: {
-            value: 0.2,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false,
-            },
-          },
-          size: {
-            value: 90,
-            random: false,
-            anim: {
-              enable: true,
-              speed: 10,
-              size_min: 40,
-              sync: false,
-            },
-          },
-          line_linked: {
-            enable: false,
+          repulse: {
             distance: 200,
-            color: "#ffffff",
-            opacity: 1,
-            width: 2,
+            duration: 0.4,
           },
-          move: {
-            enable: true,
-            speed: 8,
-            direction: "none",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 1200,
-            },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particles_nb: 2,
           },
         },
-        interactivity: {
-          detect_on: "canvas",
-          events: {
-            onhover: {
-              enable: false,
-              mode: "grab",
-            },
-            onclick: {
-              enable: false,
-              mode: "push",
-            },
-            resize: true,
-          },
-          modes: {
-            grab: {
-              distance: 400,
-              line_linked: {
-                opacity: 1,
-              },
-            },
-            bubble: {
-              distance: 400,
-              size: 40,
-              duration: 2,
-              opacity: 8,
-              speed: 3,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-            push: {
-              particles_nb: 4,
-            },
-            remove: {
-              particles_nb: 2,
-            },
-          },
-        },
-        retina_detect: true,
-      });
-    }
+      },
+      retina_detect: true,
+    });
   });
   $(window).on("load", function () {
     $(".preloader-icon").fadeOut(400);
